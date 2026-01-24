0e404d7858411b01be5c768d4b566087f8f26123
kenta-crv <okuyamakenta@gmail.com>
2026-01-24 21:23:17 +0900

    fix

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module Smart
     config.autoload_paths << Rails.root.join('app/lib')
     config.autoload_paths << Rails.root.join('app/uploaders')
     config.eager_load_paths << Rails.root.join('app/uploaders')    # Settings in config/environments/* take precedence over those specified here.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
