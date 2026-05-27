194a92620e7726bc5091d3138bd12cfa1f94b4ce
kenta-crv <okuyamakenta@gmail.com>
2026-05-27 15:46:37 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,9 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.hosts << ENV.fetch("RAILS_ALLOWED_HOST", "column.okey.work")
-  config.hosts << "j-work.jp"
-  config.hosts << "ri-plus.jp"
+  config.hosts << ENV.fetch("RAILS_ALLOWED_HOST", "okey.work")
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
